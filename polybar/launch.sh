fa813f23c4070683c551b995436c0f7e7ffc4668
Damian Regan <polycode@gmail.com>
2022-05-08 17:15:58 +0100

    feat(polybar): add font-awesome icons to stats, merge bars, clean up

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,6 +3,5 @@
 killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar left 2>&1 | tee -a /tmp/polybar-left-$m.log & disown
-    MONITOR=$m polybar right 2>&1 | tee -a /tmp/polybar-right-$m.log & disown
+    MONITOR=$m polybar main 2>&1 | tee -a /tmp/polybar-main-$m.log & disown
 done
